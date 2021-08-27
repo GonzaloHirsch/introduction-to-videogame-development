717b2d194078195dbb1bf56d45f2acba7a8f3941
Florencia Petrikovich <fpetrikovich@gmail.com>
2021-08-27 16:11:32 -0300

    [Enemy] Working enemy ship

--- tp1/unityproject/Assets/Scripts/EnemyController.cs
+++ tp1/unityproject/Assets/Scripts/EnemyController.cs
@@ -4,25 +4,41 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    private Vector3 direction;
-    private float speed = 10f;
+    public float speed = 40f;
     private int speedSign;
+    private Vector3 velocity;
     private Constants.SCREEN_BOUNDS startBound;
+    public float dtBetweenDirChanges = 2f;
+    private float timeSinceLastDirChange = 0f;
     public int scoreValue = 100;
+    private float width;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        setupSpriteSize();
+        setupStartingPosition();
+        updateVelocityVector();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Get delta time
+        float dt = Time.deltaTime;
+        // Multiply the time with the velocity to know the next position
+        transform.position += this.velocity * dt;
+        // Add to the time transcurred since last direction change
+        this.timeSinceLastDirChange += dt;
+        // If it is time to update the direction, update the velocity and
+        // reset the time transcurred counter
+        if (timeSinceLastDirChange >= dtBetweenDirChanges) {
+            updateVelocityVector();
+            this.timeSinceLastDirChange = 0;
+        }
         
     }
-    void OnTriggerEnter2D(Collider2D col)
-    {
+    void OnTriggerEnter2D(Collider2D col) {
         destroyEnemyShip();
     }
 
@@ -32,31 +48,69 @@ public class EnemyController : MonoBehaviour
         }
     }
 
+    void setupStartingPosition() {
+        // Defines if it will start on the right or left of the screen
+        this.startBound = getRandomScreenBound();
+        // Indicates the sign the y axis velocity should have
+        this.speedSign = this.startBound == Constants.SCREEN_BOUNDS.UPPER
+            ? -1
+            : 1;
+        // Adding the sprites width to the x so it is not visible upon creation
+        float deltaX = this.startBound == Constants.SCREEN_BOUNDS.UPPER
+            ? this.width
+            : -this.width;
+        
+        float x = getScreenWidthBound(this.startBound);
+        float y = Utils.getRandomNumInRange(
+            getScreenHeightBound(Constants.SCREEN_BOUNDS.LOWER), 
+            getScreenHeightBound(Constants.SCREEN_BOUNDS.UPPER)
+        );
+
+        transform.position = new Vector3(
+            x + deltaX, 
+            y,
+            0
+        );
+    }
+    void setupSpriteSize() {
+        // Recover Sprite Renderer for size
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        // Keep sizes in class
+        this.width = spriteRenderer.bounds.size.x;
+    }
+
     void destroyEnemyShip() {
         // Add the score to the counter
-        ScoreCounter.AddScore(this.scoreValue);
+        // ScoreCounter.AddScore(this.scoreValue);
         // Destroy the object when it reaches the other side
         Destroy(this.gameObject);
     }
 
     bool checkIfGoalReached() {
         // X position of the opposite side of the starting point
-        float goalX = this.getOppositeScreenWidthBound(startBound);
+        float goalX = -1 * this.getScreenWidthBound(startBound);
         return this.startBound == Constants.SCREEN_BOUNDS.UPPER
             ? this.transform.position.x <= goalX
             : this.transform.position.x >= goalX;
     }
 
-    Vector3 getRandomDirectionVector() {
-        float constXSpeed = speed;
-        return new Vector3(constXSpeed, 1, 0);
+    void updateVelocityVector() {
+        this.velocity = new Vector3(
+            this.speedSign * this.speed, 
+            this.speed * getYDirection(),
+            0
+        );
     }
 
-    void setStartingPosition() {
-        this.startBound = getRandomScreenBound();
-        this.speedSign = this.startBound == Constants.SCREEN_BOUNDS.UPPER
-            ? -1
-            : 1;
+    float getYDirection() {
+        switch(getRandomDirection()) {
+            case Constants.ENEMY_DIRECTION.DOWN:
+                return -1;
+            case Constants.ENEMY_DIRECTION.UP:
+                return 1;
+            default:
+                return 0;
+        }
     }
 
     Constants.ENEMY_DIRECTION getRandomDirection() {
@@ -71,9 +125,9 @@ public class EnemyController : MonoBehaviour
             ? Screen.height/2 
             : -Screen.height/2;
     }
-    float getOppositeScreenWidthBound(Constants.SCREEN_BOUNDS bound) {
+    float getScreenWidthBound(Constants.SCREEN_BOUNDS bound) {
         return bound == Constants.SCREEN_BOUNDS.UPPER 
-            ? -Screen.width/2 
-            : Screen.width/2;
+            ? Screen.width/2 
+            : -Screen.width/2;
     }
 }
