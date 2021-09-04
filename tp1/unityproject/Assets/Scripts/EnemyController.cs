edf2d973b982a67d5f137d629fb795539d4e6202
Florencia Petrikovich <fpetrikovich@gmail.com>
2021-09-04 12:06:54 -0300

    Enemy ships can shoot randomly. Missing accurate shooting for small ship

--- tp1/unityproject/Assets/Scripts/EnemyController.cs
+++ tp1/unityproject/Assets/Scripts/EnemyController.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    public GameObject enemyBulletPrefab;
     public float speed = 40f;
     private int speedSign;
     private Vector3 velocity;
     private Constants.SCREEN_BOUNDS startBound;
-    public float dtBetweenDirChanges = 2f;
-    private float timeSinceLastDirChange = 0f;
-    public int scoreValue = 100;
     private float width;
+    private float height;
+    private float timeSinceLastDirChange = 0f;
+    public float dtBetweenDirChanges = 2f;
+    public int scoreValue = 100;
+    public bool bulletsAreAccurate = false;
+    public float dtBetweenShooting = 1.5f;
+    public float timeSinceLastShooting = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -24,20 +29,34 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Get delta time
-        float dt = Time.deltaTime;
+        updatePosition();
+        changeDirection();
+        shoot();
+    }
+    void updatePosition() {
         // Multiply the time with the velocity to know the next position
-        transform.position += this.velocity * dt;
+        transform.position += this.velocity * Time.deltaTime;
+    }
+   
+    void updateVelocityVector() {
+        this.velocity = new Vector3(
+            this.speedSign * this.speed, 
+            this.speed * getYDirection(),
+            0
+        );
+    }
+   
+    void changeDirection() {
         // Add to the time transcurred since last direction change
-        this.timeSinceLastDirChange += dt;
+        this.timeSinceLastDirChange += Time.deltaTime;
         // If it is time to update the direction, update the velocity and
         // reset the time transcurred counter
-        if (timeSinceLastDirChange >= dtBetweenDirChanges) {
+        if (this.timeSinceLastDirChange >= this.dtBetweenDirChanges) {
             updateVelocityVector();
             this.timeSinceLastDirChange = 0;
-        }
-        
+        }   
     }
+
     void OnTriggerEnter2D(Collider2D col) {
         destroyEnemyShip();
     }
@@ -62,8 +81,8 @@ public class EnemyController : MonoBehaviour
         
         float x = getScreenWidthBound(this.startBound);
         float y = Utils.GetRandomNumInRange(
-            getScreenHeightBound(Constants.SCREEN_BOUNDS.LOWER), 
-            getScreenHeightBound(Constants.SCREEN_BOUNDS.UPPER)
+            getScreenHeightBound(Constants.SCREEN_BOUNDS.LOWER) + this.height, 
+            getScreenHeightBound(Constants.SCREEN_BOUNDS.UPPER) - this.height
         );
 
         transform.position = new Vector3(
@@ -77,6 +96,7 @@ public class EnemyController : MonoBehaviour
         SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
         // Keep sizes in class
         this.width = spriteRenderer.bounds.size.x;
+        this.height = spriteRenderer.bounds.size.y;
     }
 
     void destroyEnemyShip() {
@@ -92,14 +112,6 @@ public class EnemyController : MonoBehaviour
         return this.startBound == Constants.SCREEN_BOUNDS.UPPER
             ? this.transform.position.x <= goalX
             : this.transform.position.x >= goalX;
-    }
-
-    void updateVelocityVector() {
-        this.velocity = new Vector3(
-            this.speedSign * this.speed, 
-            this.speed * getYDirection(),
-            0
-        );
     }
 
     float getYDirection() {
@@ -129,5 +141,34 @@ public class EnemyController : MonoBehaviour
         return bound == Constants.SCREEN_BOUNDS.UPPER 
             ? Screen.width/2 
             : -Screen.width/2;
+    }
+
+    void shoot() {
+       // Add to the time transcurred since last shooting
+        this.timeSinceLastShooting += Time.deltaTime;
+        // Shoot if time transcurred is greater then the dt
+        if (this.timeSinceLastShooting >= this.dtBetweenShooting) {
+            if (this.bulletsAreAccurate) {
+                this.shootAccurately();
+            } else {
+                this.shootRandomly();
+            }
+            this.timeSinceLastShooting = 0f;
+        }
+    }
+
+    void shootAccurately() {
+        //  todo
+    }
+
+    void shootRandomly() {
+        // Bullet rotation
+        int rotAngle = (int) Utils.GetRandomNumInRange(0, 360);
+        float rotRadian = (rotAngle * Mathf.PI)/180;
+        // Bullet position
+        Quaternion rotQuaternion = Quaternion.AngleAxis(rotAngle, Vector3.right);
+        Vector3 angleVector = new Vector3(Mathf.Cos(rotRadian), Mathf.Sin(rotRadian), 0f) * this.width/2;
+        Vector3 position = angleVector + this.transform.position;
+        Instantiate(this.enemyBulletPrefab, position, rotQuaternion);
     }
 }
