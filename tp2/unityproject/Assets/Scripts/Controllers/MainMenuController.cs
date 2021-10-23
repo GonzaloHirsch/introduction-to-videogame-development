307d7b54ef4da8e5433f03052604c111d27374d4
GonzaloHirsch <hirschgonzalo@gmail.com>
2021-10-23 15:38:32 -0300

    [UI] Added controls menu

--- tp2/unityproject/Assets/Scripts/Controllers/MainMenuController.cs
+++ tp2/unityproject/Assets/Scripts/Controllers/MainMenuController.cs
@@ -1,15 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MainMenuController : MonoBehaviour
 {
+    public GameObject controlsPanel;
+    public GameObject menuPanel;
+
     void Start() {
         Cursor.lockState = CursorLockMode.None;
+        this.SeeMenu();
     }
     
     public void LoadLevel(int level) {
         GameStatus.Instance.SetLevel(level);
         SceneController.LoadGame();
+    }
+
+    public void SeeMenu(){
+        this.menuPanel.SetActive(true);
+        this.controlsPanel.SetActive(false);
+    }
+    
+    public void SeeControls(){
+        this.menuPanel.SetActive(false);
+        this.controlsPanel.SetActive(true);
     }
 }
