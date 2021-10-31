72bea484fbf8ada9216975875be1429c41e0de0a
GonzaloHirsch <hirschgonzalo@gmail.com>
2021-10-31 11:59:21 -0300

    [Maps] Added more map info

--- tp2/unityproject/Assets/Scripts/Controllers/MainMenuController.cs
+++ tp2/unityproject/Assets/Scripts/Controllers/MainMenuController.cs
@@ -4,6 +4,7 @@ public class MainMenuController : MonoBehaviour
 {
     public GameObject controlsPanel;
     public GameObject menuPanel;
+    public GameObject planPanel;
 
     void Start() {
         Cursor.lockState = CursorLockMode.None;
@@ -19,11 +20,19 @@ public class MainMenuController : MonoBehaviour
     public void SeeMenu(){
         this.menuPanel.SetActive(true);
         this.controlsPanel.SetActive(false);
+        this.planPanel.SetActive(false);
     }
     
     public void SeeControls(){
         this.menuPanel.SetActive(false);
         this.controlsPanel.SetActive(true);
+        this.planPanel.SetActive(false);
+    }
+    
+    public void SeePlan(){
+        this.menuPanel.SetActive(false);
+        this.controlsPanel.SetActive(false);
+        this.planPanel.SetActive(true);
     }
 
     private void PlayBgMusic() {
